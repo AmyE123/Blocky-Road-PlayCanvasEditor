cd12ad1adb9ff3b33f84336708e2dfe85f22fe1a
AmyE123 <amy.elliott2002@yahoo.co.uk>
2025-03-04 22:03:04 +0000

    Initial Hello World Commit

---
+++ src/helloworld.ts
@@ -0,0 +1,12 @@
+class HelloWorld extends pc.ScriptType {
+    text: string;
+    entityLink : pc.Entity | null;
+
+    initialize() {
+        console.log("Hello World!!");
+    }
+};
+
+pc.registerScript(HelloWorld, 'helloWorld');
+HelloWorld.attributes.add('text', {type: 'string'});
+HelloWorld.attributes.add('entityLink', {type : 'entity'})
